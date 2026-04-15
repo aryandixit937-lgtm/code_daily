44d007771d0ebcab88842eca82cea718db6d6787
Aryan Dixit <aryandixit937@gmail.com>
2026-04-15 22:05:57 +0530

    Create Dfs.cpp

---
+++ Dfs.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void dfs(int node, vector<vector<int>>& adj, vector<bool>& visited) {
+    visited[node] = true;
+    cout << node << " ";
+
+    for (int neighbor : adj[node]) {
+        if (!visited[neighbor]) {
+            dfs(neighbor, adj, visited);
+        }
+    }
+}
+
+int main() {
+    int n = 5; // number of nodes
+    vector<vector<int>> adj(n);
+
+    // Example graph
+    adj[0] = {1, 2};
+    adj[1] = {0, 3};
+    adj[2] = {0, 4};
+    adj[3] = {1};
+    adj[4] = {2};
+
+    vector<bool> visited(n, false);
+
+    dfs(0, adj, visited);
+}
